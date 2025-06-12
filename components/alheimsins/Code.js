3cf8c5eba12a8e945b6bac9849f09b614be65cc0
magnetixhq <ella@magnetix.ca>
2025-06-12 12:40:38 -0400

    Update Code.js
    June 12 12:40pm edits

--- components/alheimsins/Code.js
+++ components/alheimsins/Code.js
@@ -1,12 +1,12 @@
 export default ({ children }) => (
   <span className='code'>
     {children}
-    <style jsx>
-      {`
+    <style jsx>{`
+      .code {
         color: #bd10e0;
-        font-family: Menlo,Monaco,Lucida Console,Liberation Mono, DejaVu Sans Mono,Bitstream Vera Sans Mono,Courier New,monospace, serif;
+        font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif;
         font-size: 0.9em;
-      `}
-    </style>
+      }
+    `}</style>
   </span>
 )
